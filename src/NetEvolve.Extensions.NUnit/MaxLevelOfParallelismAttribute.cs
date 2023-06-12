26b199fc7111fd0e04c3c7e76cc4688548faf4fd
Martin Stühmer <3283596+samtrion@users.noreply.github.com>
2023-06-12 13:24:23 +0200

    chore: Added `MaxLevelOfParallelismAttribute`

---
+++ src/NetEvolve.Extensions.NUnit/MaxLevelOfParallelismAttribute.cs
@@ -0,0 +1,34 @@
+﻿namespace MB.DLM.API.Tests.Integration;
+
+using NUnit.Framework;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+/// <summary>
+/// Sets the number of worker threads taking into account the available
+/// <see cref="Environment.ProcessorCount"/> that can be allocated by the framework
+/// for the execution of tests.
+/// </summary>
+[AttributeUsage(AttributeTargets.Assembly, AllowMultiple = false, Inherited = false)]
+[SuppressMessage(
+    "Design",
+    "CA1019:Define accessors for attribute arguments",
+    Justification = "As designed."
+)]
+public sealed class MaxLevelOfParallelismAttribute : PropertyAttribute
+{
+    /// <summary>
+    /// Construct a <see cref="MaxLevelOfParallelismAttribute"/>.
+    /// </summary>
+    /// <param name="level">The number of worker threads to be created by the framework.</param>
+    public MaxLevelOfParallelismAttribute(int level)
+        : this(level, Environment.ProcessorCount - 1) { }
+
+    /// <summary>
+    /// Construct a <see cref="MaxLevelOfParallelismAttribute"/>.
+    /// </summary>
+    /// <param name="level">The number of worker threads to be created by the framework.</param>
+    /// <param name="alternativeLevel">The number of workers, which should be used alternatively.</param>
+    public MaxLevelOfParallelismAttribute(int level, int alternativeLevel)
+        : base("LevelOfParallelism", Math.Min(level, alternativeLevel)) { }
+}
