2f9117db44658499173bb308d16bcb4dc8ab2b64
Martin Stühmer <3283596+samtrion@users.noreply.github.com>
2023-07-05 16:41:04 +0200

    style: fixed code style

--- src/NetEvolve.Extensions.NUnit/ContinuousTestBase.cs
+++ src/NetEvolve.Extensions.NUnit/ContinuousTestBase.cs
@@ -20,7 +20,9 @@ public abstract class ContinuousTestBase
     {
         if (_stopExecution)
         {
-            Assert.Inconclusive("Previous test failed, further execution for this test class has been disabled!");
+            Assert.Inconclusive(
+                "Previous test failed, further execution for this test class has been disabled!"
+            );
         }
 
         return Task.CompletedTask;
