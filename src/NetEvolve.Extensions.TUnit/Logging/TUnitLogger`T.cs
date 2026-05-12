83ebb0ffac76a3cf07fa9b30456ad6f5aee5d2ed
Martin Stühmer <me@samtrion.net>
2026-05-12 23:38:34 +0200

    chore: Move TUnitLogger<T> to TUnitLogger.cs and delete old file
    Moved the TUnitLogger<T> implementation from TUnitLogger`T.cs into TUnitLogger.cs for consolidation. Deleted the now-empty TUnitLogger`T.cs file. No functional changes were made.

--- src/NetEvolve.Extensions.TUnit/Logging/TUnitLogger`T.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NetEvolve.Extensions.TUnit.Logging;
-
-using Microsoft.Extensions.Logging;
-
-internal sealed class TUnitLogger<T> : TUnitLogger, ILogger<T>
-{
-    public TUnitLogger(global::TUnit.Core.Logging.ILogger logger)
-        : base(logger) { }
-}
